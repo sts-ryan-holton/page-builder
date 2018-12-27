5ba71da6d50db74e70854d5681b9fe9f5fd74502
Ryan <ryan.holton@stsonline.uk.com>
2018-12-27 20:58:55 +0000

    Remove battery divs from page, update battery.js with checks

--- src/js/battery.js
+++ src/js/battery.js
@@ -1,5 +1,16 @@
 $(function() {
-  navigator.getBattery().then(function(result) {
-    console.log(result);
-  });
+  'use strict';
+  function batteryCheck() {
+    navigator.getBattery().then(function(batteryapi) {
+      var battery = batteryapi;
+      if (!battery.charging && battery.level >= 0.35 && battery.level <= 0.50) { // Not charging & 45% battery
+        swal("Battery Level Low", "Your battery level is getting low, we recommend building a page whilst connected to a power supply.", "warning");
+      } else if (!battery.charging && battery.level <= 0.34) {
+        swal("Battery Level Critical", "We don't recommend building a page with this battery level percentage.", "error");
+      }
+    });
+  }
+  if ( $('body').length ) {
+    batteryCheck();
+  }
 });
